e66ec2bdf188c6f244ae8e4fb9cca71a0f06b4a2
blackd0t <blackd0t@protonmail.com>
2021-05-29 00:21:12 +0300

    🧪 TEST: esting arduino ci

--- src/hardware/electricalcompute/voting/src/main.cpp
+++ src/hardware/electricalcompute/voting/src/main.cpp
@@ -1,9 +1,23 @@
 #include <Arduino.h>
 
-void setup() {
-  // put your setup code here, to run once:
+void setup()
+{
+	// put your setup code here, to run once:
+	Serial.begin(115200);
+	// initialize LED digital pin as an output.
+	pinMode(4, OUTPUT);
 }
 
-void loop() {
-  // put your main code here, to run repeatedly:
+void loop()
+{
+	// put your main code here, to run repeatedly:
+	Serial.println("Hello world!");
+	// turn the LED on (HIGH is the voltage level)
+	digitalWrite(4, HIGH);
+	// wait for a second
+	delay(1000);
+	// turn the LED off by making the voltage LOW
+	digitalWrite(4, LOW);
+	// wait for a second
+	delay(1000);
 }
---
+++ src/hardware/electricalcompute/voting/test/test_main.cpp
@@ -0,0 +1,47 @@
+#include <Arduino.h>
+#include <unity.h>
+
+// void setUp(void) {
+// // set stuff up here
+// }
+
+// void tearDown(void) {
+// // clean stuff up here
+// }
+
+void test_led_state_high(void) {
+    digitalWrite(4, HIGH);
+    TEST_ASSERT_EQUAL(HIGH, digitalRead(4));
+}
+
+void test_led_state_low(void) {
+    digitalWrite(4, LOW);
+    TEST_ASSERT_EQUAL(LOW, digitalRead(4));
+}
+
+void setup() {
+    // NOTE!!! Wait for >2 secs
+    // if board doesn't support software reset via Serial.DTR/RTS
+    delay(2000);
+
+    UNITY_BEGIN();    // IMPORTANT LINE!
+
+    pinMode(4, OUTPUT);
+}
+
+uint8_t i = 0;
+uint8_t max_blinks = 5;
+
+void loop() {
+    if (i < max_blinks)
+    {
+        RUN_TEST(test_led_state_high);
+        delay(500);
+        RUN_TEST(test_led_state_low);
+        delay(500);
+        i++;
+    }
+    else if (i == max_blinks) {
+      UNITY_END(); // stop unit testing
+    }
+}
