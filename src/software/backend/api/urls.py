257f6be3fb6859f6c1ab34d65525c8113cf72ceb
ObaraEmmanuel <emmanuelobarany@gmail.com>
2021-07-02 21:13:50 +0300

    Add jwt login and refresh endpoint

--- src/software/backend/api/urls.py
+++ src/software/backend/api/urls.py
@@ -1,8 +1,9 @@
 from django.urls import path, include
-from .views import UserList
 from rest_framework_simplejwt import views as jwt_views
 
 
 urlpatterns = [
-    path('auth/signup/', include('dj_rest_auth.registration.urls')),
+    path('auth/signup/', include('dj_rest_auth.registration.urls'), name='signup'),
+    path('auth/login/', jwt_views.TokenObtainPairView.as_view(), name='login'),
+    path('auth/refresh/', jwt_views.TokenRefreshView.as_view(), name='refresh')
 ]
